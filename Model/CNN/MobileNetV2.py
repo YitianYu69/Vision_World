67dec6ca115f41fb125d04eda7eb5192a9240495
Yitian Yu <YitianYuyyds@outlook.com>
2025-09-02 07:57:08 -0400

    Finished the implementation of MobileNetV2

--- Model/CNN/MobileNetV2.py
+++ Model/CNN/MobileNetV2.py
@@ -1,13 +1,16 @@
 import torch
 from torch import nn
 
-def make_divisor(value, divisor, min_value=None):
-    new_value = max(min_vale, int(value + divisor / 2)  // divisor * divisor)
+def make_divisible(value, divisor, min_value=None):
+    if min_value is None:
+        min_value = divisor 
+
+    new_value = max(min_value, int(value + divisor / 2)  // divisor * divisor)
     if new_value < 0.9 * value:
         new_value += divisor
     return new_value
 
-def Conv3x3(in_planes, planes, stride):
+def Conv_3x3(in_planes, planes, stride):
     return nn.Sequential(
         nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False),
         nn.BatchNorm2d(planes),
@@ -15,7 +18,7 @@ def Conv3x3(in_planes, planes, stride):
     )
 
 
-def Conv1x1(in_planes, planes):
+def Conv_1x1(in_planes, planes):
     return nn.Sequential(
         nn.Conv2d(in_planes, planes, kernel_size=1, stride=1, bias=False),
         nn.BatchNorm2d(planes),
@@ -27,16 +30,16 @@ class InvertedResidual(nn.Module):
         super().__init__()
 
         hidden_dim = int(in_planes * expand_ratio)
-        self.residual = stride == 1
+        self.residual = (stride == 1 and in_planes == planes)
 
         if in_planes == hidden_dim:
             self.conv = nn.Sequential(
                 # dw conv
-                nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride, padding=1, groups=planes, bias=False),
-                nn.BatchNorm2d(planes),
+                nn.Conv2d(in_planes, in_planes, kernel_size=3, stride=stride, padding=1, groups=in_planes, bias=False),
+                nn.BatchNorm2d(in_planes),
                 nn.ReLU6(),
                 # pw conv
-                nn.Conv2d(planes, planes, kernel_size=1, stride=1, bias=False),
+                nn.Conv2d(in_planes, planes, kernel_size=1, stride=1, bias=False),
                 nn.BatchNorm2d(planes)
             )
         else:
@@ -48,10 +51,49 @@ class InvertedResidual(nn.Module):
                 nn.BatchNorm2d(hidden_dim),
                 nn.ReLU6(),
                 nn.Conv2d(hidden_dim, planes, kernel_size=1, stride=1, bias=False),
-                nn.BatcjNorm2d(planes)
+                nn.BatchNorm2d(planes)
             )
     
     def forward(self, x):
         if self.residual:
             return x + self.conv(x)
-        return x
+        return self.conv(x)
+
+    
+class MobileNetV2(nn.Module):
+    def __init__(self, num_classes, width_mul=1.0):
+        super().__init__()
+
+        self.cfgs = [
+            # t, c, n, s
+            [1,  16, 1, 1],
+            [6,  24, 2, 2],
+            [6,  32, 3, 2],
+            [6,  64, 4, 2],
+            [6,  96, 3, 1],
+            [6, 160, 3, 2],
+            [6, 320, 1, 1],
+        ]
+
+        layers = []
+        in_planes = make_divisible(32 * width_mul, 4 if width_mul == 0.1 else 8)
+        layers += [Conv_3x3(3, in_planes, 2)]
+
+        for t, c, n, s in self.cfgs:
+            out_planes = make_divisible(c * width_mul, 4 if width_mul == 0.1 else 8)
+            for i in range(n):
+                stride = s if i == 0 else 1
+                layers += [InvertedResidual(in_planes, out_planes, stride=stride, expand_ratio=t)]
+                in_planes = out_planes
+        
+        final_out_planes = make_divisible(1280 * width_mul, 8) if width_mul > 1.0 else 1280
+        layers += [Conv_1x1(in_planes, final_out_planes)]
+        self.feature_forward = nn.Sequential(*layers)
+        self.avg = nn.AdaptiveAvgPool2d(1)
+        self.classifier = nn.Linear(final_out_planes, num_classes)
+
+    def forward(self, x):
+        x = self.feature_forward(x)
+        x = self.avg(x)
+        x =  x.view(x.size(0), -1)
+        return self.classifier(x)
