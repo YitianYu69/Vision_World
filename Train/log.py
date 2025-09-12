5dc74f733d49fd8507887c3219c8aa52aa3435ad
Yitian Yu <YitianYuyyds@outlook.com>
2025-09-12 16:17:22 -0400

    Implemeented aa log file

---
+++ Train/log.py
@@ -0,0 +1,34 @@
+import logging
+import sys
+import os
+from datetime import datetime
+
+def get_logger():
+    file_name = os.path.splitext(os.path.basename(sys.argv[0]))[0]
+    log_file = f"{file_name}_{datetime.now().strftime('%Y%m%d_%H%M%S')}.log"
+
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+
+    for h in logger.handlers[:]:
+        logger.removeHandler(h)
+
+    console_handler = logging.StreamHandler(sys.stdout)
+    console_handler.setLevel(logging.INFO)
+
+    file_handler = logging.FileHandler(log_file, mode="w")
+    file_handler.setLevel(logging.INFO)
+
+    formatter = logging.Formatter(
+        "[%(asctime)s] [%(levelname)s] [%(filename)s] %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S"
+    )
+
+    console_handler.setFormatter(formatter)
+    file_handler.setFormatter(formatter)
+
+    logger.addHandler(console_handler)
+    logger.addHandler(file_handler)
+    return logger
+
+
